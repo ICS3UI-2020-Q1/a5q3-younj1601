b4b06fc14ffc031daca73932f9fc5f8f6b571b13
younj1601 <younj1601@wrdsb.ca>
2020-10-06 15:24:27 +0000

    Finished the third question for 2.03

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Calculate the factorial of an inserted number
+ * @author Jackson
  */
 public class Main {
 
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //scanner for input
+    Scanner input = new Scanner(System.in);
+
+    //get the interger
+    System.out.println("Please enter an integer to calculate the factorial of");
+    int num = input.nextInt();
+
+    //declare factorial variable
+    int factorial = 1;
+
+    //create for loop for calculating factorial
+    for(int count = 1; count <= num; count++){
+      factorial = count*factorial;
+
+    }
+    System.out.println(num + "! = " + factorial);
   }
 }
